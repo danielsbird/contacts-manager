ae0dd855bbf8a9e50574c5dcbcce83c922775196
Dan Bird <danielsbird@gmail.com>
2015-05-05 10:58:22 -0400

    Add semi-permanent sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,32 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     has_secure_password
     has_many :contact
+
+    # Returns the hash digest of a given string
+    def User.digest(string)
+        cost = BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Store hashed remember token in database
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Compares remember token in cookies with hashed remember token in database
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Removes remember digest from database
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504192952) do
+ActiveRecord::Schema.define(version: 20150505140322) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150504192952) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
