c03f066971e86d96a3e8b1af155f594c26c3a5ea
Dan Bird <danielsbird@gmail.com>
2015-05-05 15:29:36 -0400

    Applied bootstrap styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @contacts = @user.contact
+        @contacts = @user.contact.order('last_name')
     end
 
     private
