aba612252f103e5d92823d6de7a65ba3ac1a29a8
Dan Bird <danielsbird@gmail.com>
2015-05-07 10:20:56 -0400

    Use gravatar_image_tag gem

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,8 +1,6 @@
 module ContactsHelper
 
     def gravatar_for(contact)
-        gravatar_id = Digest::MD5::hexdigest(contact.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tag(gravatar_url, alt: "#{contact.first_name} #{contact.last_name}", class: "gravatar")
+        gravatar_image_tag(@contact.email, :alt => "#{@contact.first_name} #{@contact.last_name}")
     end
 end
